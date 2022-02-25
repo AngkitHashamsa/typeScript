c75ee52d8ef926e2711c149e14d744e70aade686
Angkit <angkithashamsa21@gmail.com>
2022-02-26 00:36:25 +0530

    more of the code

--- dist/Index.js
+++ dist/Index.js
@@ -66,3 +66,53 @@ user123["Working"] = true;
 const addNum = (x, y) => x + y;
 const sub = (x, y) => x - y;
 // console.log(addNum(1, 3));
+// !CLASSES
+class Person {
+    constructor(name, id, age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+    register() {
+        return `${this.name} is now register`;
+    }
+}
+//! Property 'id' is private and only accessible within class 'Person'.
+// ? Property 'age' is protected and only accessible within class 'Person' and its subclasses.
+const user1 = new Person("Angkit", 23, 26);
+const user2 = new Person("hurray", 25, 30);
+class Person2 {
+    constructor(name, id, age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+    register() {
+        return `${this.name} is now register`;
+    }
+}
+const user3 = new Person2("Angkit2155", 24, 45);
+// console.log(user3);
+class Employee extends Person2 {
+    constructor(name, id, age, position) {
+        super(name, id, age);
+        this.position = position;
+    }
+}
+const newEmployee = new Employee("Raul", 34, 25, "Manager");
+// console.log(newEmployee);
+// * Generics
+function getArray(item) {
+    return new Array().concat(item);
+}
+let numArray = getArray([1, 2, 3, 4, 5]);
+console.log(numArray);
+let stringArray = getArray(["apple", "banana", "carrot"]);
+stringArray.push("tiger");
+console.log(stringArray);
+function getArrayWithType(item) {
+    return new Array().concat(item);
+}
+let numArrayWithType = getArrayWithType([1, 34, 54, 3]);
+let stringArrayWithType = getArrayWithType(["one", "two", "three"]);
+// stringArrayWithType.push('onetwo')
--- src/Index.ts
+++ src/Index.ts
@@ -112,3 +112,82 @@ interface MathFunc {
 const addNum: MathFunc = (x: number, y: number): number => x + y;
 const sub: MathFunc = (x: number, y: number): number => x - y;
 // console.log(addNum(1, 3));
+
+// !CLASSES
+
+class Person {
+  private id: number;
+  name: string;
+  protected age: number;
+  constructor(name: string, id: number, age: number) {
+    this.name = name;
+    this.id = id;
+    this.age = age;
+  }
+  register() {
+    return `${this.name} is now register`;
+  }
+}
+//! Property 'id' is private and only accessible within class 'Person'.
+// ? Property 'age' is protected and only accessible within class 'Person' and its subclasses.
+
+const user1 = new Person("Angkit", 23, 26);
+const user2 = new Person("hurray", 25, 30);
+//! user1.id = 12343434; we wont be able to assign
+// console.log(user1, user2);
+// console.log(user1.id); we cant acces id as it is priavte
+// user1.id === 234;
+// console.log(Object.keys(brad));
+// console.log(user1.register());
+
+interface PersonInterface {
+  id: number;
+  name: string;
+  age: number;
+  register(): string;
+}
+class Person2 implements PersonInterface {
+  id: number;
+  name: string;
+  age: number;
+  constructor(name: string, id: number, age: number) {
+    this.name = name;
+    this.id = id;
+    this.age = age;
+  }
+  register() {
+    return `${this.name} is now register`;
+  }
+}
+const user3 = new Person2("Angkit2155", 24, 45);
+// console.log(user3);
+
+class Employee extends Person2 {
+  position: string;
+  constructor(name: string, id: number, age: number, position: string) {
+    super(name, id, age);
+    this.position = position;
+  }
+}
+const newEmployee = new Employee("Raul", 34, 25, "Manager");
+// console.log(newEmployee);
+
+// * Generics
+
+function getArray(item: any[]): any[] {
+  return new Array().concat(item);
+}
+
+let numArray = getArray([1, 2, 3, 4, 5]);
+console.log(numArray);
+let stringArray = getArray(["apple", "banana", "carrot"]);
+stringArray.push("tiger");
+console.log(stringArray);
+
+function getArrayWithType<T>(item: T[]): T[] {
+  return new Array().concat(item);
+}
+
+let numArrayWithType = getArrayWithType<number>([1, 34, 54, 3]);
+let stringArrayWithType = getArrayWithType<string>(["one", "two", "three"]);
+// stringArrayWithType.push('onetwo')
